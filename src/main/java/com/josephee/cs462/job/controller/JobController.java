9b29aa997e8c366a6265c99f3f8efe506367f2ff
Joseph Cotton <jtcotton63@gmail.com>
2017-07-03 16:32:42 -0600

    Added updated job code and event

--- src/main/java/com/josephee/cs462/job/controller/JobController.java
+++ src/main/java/com/josephee/cs462/job/controller/JobController.java
@@ -5,10 +5,7 @@ import com.josephee.cs462.job.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/jobs")
@@ -21,6 +18,13 @@ public class JobController {
     public Long create(@RequestBody JobModel model) {
         JobModel saved = jobService.create(model);
         return saved.getId();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Long update(@PathVariable Long id, @RequestBody JobModel model) {
+        model.setId(id);
+        JobModel updated = jobService.update(model);
+        return updated.getId();
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
--- src/main/java/com/josephee/cs462/job/service/JobService.java
+++ src/main/java/com/josephee/cs462/job/service/JobService.java
@@ -2,6 +2,7 @@ package com.josephee.cs462.job.service;
 
 import com.josephee.cs462.common.dispatch.JobEventsDispatcher;
 import com.josephee.cs462.common.model.event.CreateEvent;
+import com.josephee.cs462.common.model.event.UpdatedEvent;
 import com.josephee.cs462.job.domain.Job;
 import com.josephee.cs462.common.model.job.JobModel;
 import com.josephee.cs462.job.repository.JobRepository;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +25,26 @@ public class JobService {
     @Autowired
     private JobEventsDispatcher dispatcher;
 
-    public JobModel create(JobModel incoming) {
-        incoming.setId(null);
+    private JobModel save(JobModel incoming) {
         Job toBeSaved = new Job(incoming);
         Job saved = jobRepository.save(toBeSaved);
         JobModel toBeReturned = saved.toModel();
+        return toBeReturned;
+    }
+
+    public JobModel create(JobModel incoming) {
+        incoming.setId(null);
+        JobModel toBeReturned = save(incoming);
         dispatcher.jobCreated(new CreateEvent<>(toBeReturned));
+        return toBeReturned;
+    }
+
+    public JobModel update(JobModel incoming) {
+        Assert.notNull(incoming);
+        JobModel existing = jobRepository.getOne(incoming.getId()).toModel();
+        Assert.notNull(existing);
+        JobModel toBeReturned = save(incoming);
+        dispatcher.jobUpdated(new UpdatedEvent<>(existing, toBeReturned));
         return toBeReturned;
     }
 
